5351923547ce7a160d389cdcc0b10ab30be5af9c
Ha Tran <haiha262@gmail.com>
2018-05-24 13:49:20 +1000

    try

--- js/spirit_script.js
+++ js/spirit_script.js
@@ -1,5 +1,5 @@
+$(document).ready(function() {
 
-$( document ).ready(function() {
 var rad = document.getElementsByName("c_000000da");
     var prev = null;
     for(var i = 0; i < rad.length; i++) {
@@ -11,5 +11,14 @@ var rad = document.getElementsByName("c_000000da");
             console.log(this.value)
         };
     }
+$( "<h1>HELLO</h1>" ).appendTo( $( "body" ) );
 
+$('input[type=radio][name=c_000000da]').change(function() {
+        if (this.value == 'allot') {
+            alert("Allot Thai Gayo Bhai");
+        }
+        else if (this.value == 'transfer') {
+            alert("Transfer Thai Gayo");
+        }
+    });
 });
